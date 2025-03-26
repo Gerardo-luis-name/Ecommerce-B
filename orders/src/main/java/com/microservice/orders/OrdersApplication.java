33c53d470edc8034bb4042ee1f6c7e0f07b38ae5
Marisol Reyes <reyessanchezmarisol5@gmail.com>
2025-03-26 13:44:12 -0600

    Modificacion en OrdersApplication

--- orders/src/main/java/com/microservice/orders/OrdersApplication.java
+++ orders/src/main/java/com/microservice/orders/OrdersApplication.java
@@ -2,8 +2,12 @@ package com.microservice.orders;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
+@EntityScan(basePackages = "com.ecommerce.commons.models.entity")
 public class OrdersApplication {
 
 	public static void main(String[] args) {
